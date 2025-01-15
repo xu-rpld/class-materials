61d770ccadaa54f6a37c29deb7b19d028681386f
Jonathan Pickard <jpickard@analyzer1.com>
2025-01-15 18:01:58 -0500

    feat (RC-2) Add Example Sprint Teams

---
+++ examples/sprint_teams/src/main.rs
@@ -0,0 +1,67 @@
+use rand::Rng;
+
+struct SprintTeam {
+    name: String,
+    spaces_remaining: u8,
+} 
+
+fn main() {
+    let students = [
+        "Simon William Benjamin",
+        "Julian A Brito-Hanley",
+        "Nick Clarkson",
+        "Sam Ethan Fortin",
+        "Jack Allen Frambes",
+        "Evan Thomas Gunnulfsen",
+        "Jack Robert Hoeting",
+        "Julia Lacy Hootman",
+        "Calvin William LaHuis",
+        "Adam Joseph Maida",
+        "Brian Donovan Matton",
+        "Katelyn Quach-Giang",
+        "Gavin Ryan Reed",
+        "Nicolas Frank Salvador",
+        "Cory Davis Schaefer",
+        "Carmen Atticus Shero",
+        "Lance Stephen Silliman",
+        "Kyle R Totorica",
+        "Khanh Tran",
+        "Alex K Welch",
+    ];
+  
+    let sprint_team_a  = SprintTeam { 
+        name: "Team A".to_string(), 
+        spaces_remaining: 7,
+    };
+
+    let sprint_team_b = SprintTeam { 
+        name: "Team B".to_string(), 
+        spaces_remaining: 7,
+    };
+
+    let sprint_team_c = SprintTeam { 
+        name: "Team C".to_string(), 
+        spaces_remaining: 6,
+    };
+
+    let mut teams = [sprint_team_a, sprint_team_b, sprint_team_c];
+
+    let mut rng = rand::thread_rng();
+
+    for student in students {
+        loop {
+            let random_number = rng.gen_range(0..teams.len());
+            let team = &mut teams[random_number];
+            if team.spaces_remaining > 0 {
+                team.spaces_remaining -= 1;
+                println!(
+                    "Student: {:<30}  Team: {:<10}  Remaining Spaces: {}",
+                    student,
+                    team.name,
+                    team.spaces_remaining
+                );
+                break;
+            }
+        }
+    }
+}
